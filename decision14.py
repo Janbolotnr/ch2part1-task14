820a6f57d11224e1bdbe5ab5861c5c6ad34cb10b
Janbolotnr <janbolotnr@gmail.com>
2020-03-08 01:37:13 +0600

    ch2task14

---
+++ decision14.py
@@ -0,0 +1,17 @@
+number = int(input("numbers: "))
+for i in range(number):
+    if int(i)%2 == 0:
+        print(i, end=' ')
+        
+
+
+
+
+#number = int(input("Enter: "))
+#if number % 2 == 0:
+ #   print("chetnoe")
+#lse:
+ #   print("False")    
+
+
+
